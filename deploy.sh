0a3f491775ba781410888b7ae609a91a7bcc0220
Gabriel Petrovay <gabipetrovay@gmail.com>
2016-06-30 17:10:07 +0200

    Added resource limits to pods

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 
-kubectl create -f gce-volumes.yaml
-kubectl create secret generic mysql-pass --from-file=password.txt
-kubectl create -f mysql-deployment.yaml
-kubectl create -f prestashop-deployment.yaml
+if [ "$1" ]
+then
+    PREFIX="$1-"
+fi
+
+gcloud compute disks create "${PREFIXi}prestashop-1" --size "10"
+gcloud compute disks create "${PREFIXi}prestashop-2" --size "10"
+
+kubectl create -f "${PREFIX}gce-volumes.yaml"
+kubectl create secret generic "${PREFIX}mysql-pass" --from-file=password.txt
+kubectl create -f "${PREFIX}mysql-deployment.yaml"
+kubectl create -f "${PREFIX}prestashop-deployment.yaml"
