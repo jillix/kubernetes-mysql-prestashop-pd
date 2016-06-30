1265f0a5fbd39f06ef8422362767fb1c5e73041d
Gabriel Petrovay <gabipetrovay@gmail.com>
2016-06-30 17:37:47 +0200

    Allowing prefixed scripts for multiple deployments

--- delete.sh
+++ delete.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 
-kubectl delete -f gce-volumes.yaml
-kubectl delete secret mysql-pass
-kubectl delete -f mysql-deployment.yaml
-kubectl delete -f prestashop-deployment.yaml
+if [ "$1" ]
+then
+    PREFIX="$1-"
+fi
+
+gcloud compute disks delete "${PREFIX}prestashop-1"
+gcloud compute disks delete "${PREFIX}prestashop-2"
+
+kubectl delete -f "${PREFIX}gce-volumes.yaml"
+kubectl delete secret "${PREFIX}mysql-pass"
+kubectl delete -f "${PREFIX}mysql-deployment.yaml"
+kubectl delete -f "${PREFIX}prestashop-deployment.yaml"
--- deploy.sh
+++ deploy.sh
@@ -5,8 +5,8 @@ then
     PREFIX="$1-"
 fi
 
-gcloud compute disks create "${PREFIXi}prestashop-1" --size "10"
-gcloud compute disks create "${PREFIXi}prestashop-2" --size "10"
+gcloud compute disks create "${PREFIX}prestashop-1" --size "10"
+gcloud compute disks create "${PREFIX}prestashop-2" --size "10"
 
 kubectl create -f "${PREFIX}gce-volumes.yaml"
 kubectl create secret generic "${PREFIX}mysql-pass" --from-file=password.txt
